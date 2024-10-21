1ee40c8bcce12b9b6cd9f580db92db69ee9a8bbb
ngaharon <ngairaharon1@gmail.com>
2024-10-21 22:01:51 +0300

    Navbar Section

--- crafter-hub-website/app/page.tsx
+++ crafter-hub-website/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Navbar from "@/components/navbar";
+import { Spotlight } from "@/components/ui/spotlight";
 
 export default function Home() {
   return (
-    <div>
-      hii
+    <div className="w-full h-screen md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.2] relative overflow-hidden">
+      <Navbar />
+      
     </div>
   );
 }
